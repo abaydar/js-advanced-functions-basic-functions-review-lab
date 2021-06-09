1214e7cbdc2a92fea4f81f8fc8b76a69d6176006
+ <+>
2021-06-09 17:40:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Your code here
+function saturdayFun(task="roller-skate"){
+    return `This Saturday, I want to ${task}!`
+}
+
+function mondayWork(task="go to the office"){
+    return `This Monday, I will ${task}.`
+}
+
+function wrapAdjective(flair="*"){
+    return function(adj="special"){
+        return `You are ${flair}${adj}${flair}!`
+    }
+}
+
+let Calculator = {
+    add: function(a, b){
+        return a + b
+    },
+    subtract: function(a,b){
+        return a - b
+    },
+    multiply: function(a,b){
+        return a * b
+    },
+    divide: function(a,b){
+        return a/b
+    }
+}
+
+function actionApplyer(integer, array){
+    let a = integer
+
+    for (let i=0; i<array.length; i++){
+        a = array[i](a)
+    }
+
+    return a
+}
